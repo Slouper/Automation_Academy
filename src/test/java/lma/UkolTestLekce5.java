20add88730168f68a3cf2941155a79641d769b8d
maja93cz <l.maier1993@gmail.com>
2019-03-11 10:39:41 +0100

    LMA: Adding HomeWorks + Lesson 2

---
+++ src/test/java/lma/UkolTestLekce5.java
@@ -0,0 +1,84 @@
+package lma;
+
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.*;
+import org.openqa.selenium.interactions.Actions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Test
+public class UkolTestLekce5 {
+    public ChromeDriver driver;
+    private static final Logger log = LoggerFactory.getLogger(UkolTestLekce5.class);
+
+
+    @BeforeMethod
+    public void getChromeDriver() {
+        System.setProperty("webdriver.chrome.driver", "C:/chrome_driver/chromedriver.exe");
+
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
+    }
+    @Test
+    public void UkolLekce4() {
+        Actions action = new Actions(driver);
+
+        log.debug("Test 'UkolTestLekce5' started");
+
+        driver.get("http://www.redmine.org/");
+
+        driver.findElement(By.className("register")).click();
+
+        log.debug("Register started....");
+
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"content\"]/h2")));
+
+        driver.findElement(By.id("user_login")).sendKeys("Java1234567");
+        driver.findElement(By.id("user_password")).sendKeys("Heslo123");
+        driver.findElement(By.id("user_password_confirmation")).sendKeys("Heslo123");
+        driver.findElement(By.id("user_firstname")).sendKeys("Krestni");
+        driver.findElement(By.id("user_lastname")).sendKeys("Prijmeni");
+        driver.findElement(By.id("user_mail")).sendKeys("eee@fff.cz");
+        driver.findElement(By.id("user_language")).click();
+        driver.findElement(By.xpath("//*[@id=\"user_language\"]/option[13]")).click();
+        driver.findElement(By.id("user_custom_field_values_3")).sendKeys("123456");
+
+        action
+                .sendKeys(Keys.ENTER)
+                .perform();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"flash_notice\"]")));
+
+        log.debug("Login started....");
+
+        driver.findElement(By.id("username")).sendKeys("Java1234567");
+        driver.findElement(By.id("password")).sendKeys("Heslo123");
+
+        action
+                .sendKeys(Keys.ENTER)
+                .perform();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"flash_error\"]")));
+
+        log.debug("You haven't activated your account yet. If you want to receive a new activation email, please click this link.");
+    }
+
+
+    @AfterMethod
+    public void endDriver() {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        driver.quit();
+    }
+}
