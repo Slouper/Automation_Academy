5df836e10a209ec44435d6f3eafa062e806cac59
Petr Fifka <petr.fifka@inventi.cz>
2019-03-20 11:14:17 +0100

    PFI: xpath hodina

--- src/test/java/sloup/D_xpath_lecture_7_test/AbstractTestXpath.java
+++ src/test/java/sloup/D_xpath_lecture_7_test/AbstractTestXpath.java
@@ -1,17 +1,17 @@
-package sloup.C_bestpractice_lecture_6.lecture;
+package sloup.D_xpath_lecture_7_test;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
-public abstract class AbstractTest {
+public abstract class AbstractTestXpath {
     private WebDriver driver;
 
     @BeforeClass
     public void startDriver() {
+        System.setProperty("webdriver.chrome.driver", "C:\\chrome_driver\\chromedriver.exe");
         driver = new ChromeDriver();
-
     }
 
     @AfterClass
