37f20b78ee99cbaed89e898f85d46eb42ede34b4
Sedymova Alina <alina_sedymova@kb.cz>
2019-05-26 14:30:33 +0200

    Add final versions of cv and du for lection 2

--- src/test/java/ASE/Lekce2/cv/AseTestLekce2.java
+++ src/test/java/ASE/Lekce2/cv/AseTestLekce2.java
@@ -1,4 +1,61 @@
 package ASE.Lekce2.cv;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+@Test
 public class AseTestLekce2 {
+    private ChromeDriver driver;
+
+    @BeforeTest
+    public void beforeTest() {
+        driver = new ChromeDriver();
+        driver.get("https://www.seznam.cz");
+    }
+
+    @AfterTest
+    public void afterTest() {
+        driver.quit();
+
+//        exception handling (bad practise)
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Test
+    public void SearchIntoBing() {
+
+//        1) WebElement searchInput
+        WebElement searchInput = driver.findElement(By.xpath("//input[@name='q']"));
+        searchInput.sendKeys("Selenium Webdriver");
+
+//        2) submit = like enter, click for searching
+//        WebElement searchButton = driver.findElementByXPath("//form//button[contains(text(),'Vyhledat')]");
+//        searchButton.click(); NEBO
+        searchInput.submit();
+
+//        3) wait for element
+        WebDriverWait wait = new WebDriverWait(driver, 15);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@title='Vyhledat']")));
+
+//        4) variable searchButtonMagnifier
+        WebElement searchButtonMagnifier = driver.findElementByXPath("//button[@title='Vyhledat']");
+
+//        5) display in the console, sout => System.out.println();
+        System.out.println(searchButtonMagnifier.getAttribute("title"));
+
+
+    }
+
 }
+
--- src/test/java/ASE/Lekce2/du/AseTestLekce2_DU.java
+++ src/test/java/ASE/Lekce2/du/AseTestLekce2_DU.java
@@ -1,4 +1,67 @@
 package ASE.Lekce2.du;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.GeckoDriverService;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+@Test
 public class AseTestLekce2_DU {
+//    private ChromeDriver driver;
+    private FirefoxDriver driver;
+    private WebDriverWait wait;
+
+    @BeforeTest
+    public void beforeTest() {
+//        driver = new ChromeDriver();
+        System.setProperty("webdriver.gecko.driver", "c:\\Users\\alina.sedymova\\IdeaProjects\\Drivers\\geckodriver.exe");
+        driver = new FirefoxDriver();
+        driver.get("https://www.seznam.cz");
+        wait = new WebDriverWait(driver, 15);
+    }
+
+    @AfterTest
+    public void afterTest() {
+        driver.quit();
+
+//        exception handling (bad practise)
+//        try {
+//            Thread.sleep(3000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+
+    }
+
+    @Test
+    public void SearchIntoBing() {
+
+//        1) WebElement searchInput
+        WebElement searchInput = driver.findElement(By.xpath("//input[@name='q']"));
+        searchInput.sendKeys("Selenium Webdriver");
+
+//        2) submit = like enter, click for searching
+//        WebElement searchButton = driver.findElementByXPath("//form//button[contains(text(),'Vyhledat')]");
+//        searchButton.click(); NEBO
+        searchInput.submit();
+
+//        3) wait for element
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@title='Vyhledat']")));
+
+//        4) variable searchButtonMagnifier
+        WebElement searchButtonMagnifier = driver.findElementByXPath("//button[@title='Vyhledat']");
+
+//        5) display in the console, sout => System.out.println();
+        System.out.println(searchButtonMagnifier.getAttribute("title"));
+
+
+    }
+
 }
+
