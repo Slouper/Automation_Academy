cb892be69be73ebf0c977262374eda7d61a9a593
Jan Königsmark <j.konigsmark@gmail.com>
2019-05-29 18:21:46 +0200

    added lekce homework

---
+++ src/test/java/jko/lekce2/cv/SpaghettiTest.java
@@ -0,0 +1,56 @@
+package jko.lekce2.cv;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+@Test
+public class SpaghettiTest {
+
+    private ChromeDriver driver;
+
+    @BeforeTest
+    public void beforeTest() {
+
+        driver = new ChromeDriver();
+        driver.get("https://www.seznam.cz");
+
+    }
+
+    @AfterTest
+    public void AfterTest() {
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        driver.quit();
+
+    }
+
+    @Test
+    public void bingSearch() {
+
+        WebElement searchInput = driver.findElementByXPath("//*[@name='q']");
+
+        searchInput.sendKeys("Selenium Webdriver");
+
+        WebElement submitButton = driver.findElementByXPath("//button[@type='submit' and contains(@class, 'search')]");
+
+        submitButton.click();
+
+        WebDriverWait wait = new WebDriverWait(driver, 15);
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@title='Vyhledat']")));
+        System.out.println();
+    }
+}
